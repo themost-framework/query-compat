d8f9575e178745fb363d166241185471ec97430a
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2023-10-21 07:38:13 +0000

    update header

--- closures/src/PrototypeMethodParser.d.ts
+++ closures/src/PrototypeMethodParser.d.ts
@@ -1,3 +1,3 @@
-// MOST Web Framework Codename Zero Gravity Copyright (c) 2017-2022, THEMOST LP All rights reserved
+// MOST Web Framework Codename Zero Gravity Copyright (c) 2017-2023, THEMOST LP All rights reserved
 
 export declare class PrototypeMethodParser {}
